5ca2b9e29291906316458448a37ace19da228d0f
Sami Akçay <sami.akcay55@outlook.com>
2024-05-27 22:15:39 +0300

    user.cs eklendi

---
+++ CampusRide/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace CampusRide.Models
+{
+    public class User
+    {
+        public ulong Id { get; set; }
+        public string? Name { get; set; } = string.Empty;
+        public string? Surname { get; set; } = string.Empty;
+        public string? FullName { get; } 
+        public string? Email { get; set; } = string.Empty;
+        public DateTime RegistryTime { get; set; }
+        public User()
+        {
+            RegistryTime = DateTime.Now;
+            FullName = Name + Surname;
+        }
+    }
+}
