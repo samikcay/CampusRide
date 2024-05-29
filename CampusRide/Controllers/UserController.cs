2ddde92799122cbdd769687dfc978636af7d9365
Sami Akçay <sami.akcay55@outlook.com>
2024-05-29 23:26:23 +0300

    ufakcıkcık değişiklikler

--- CampusRide/Controllers/UserController.cs
+++ CampusRide/Controllers/UserController.cs
@@ -8,5 +8,10 @@ namespace CampusRide.Controllers
         {
             return View();
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
