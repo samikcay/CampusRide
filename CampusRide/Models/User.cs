c3b6cd6e36d742dc037e3161bf0e3feadcfaf8dc
Sami Akçay <sami.akcay55@outlook.com>
2024-06-08 23:07:16 +0300

    Login ve Register güncellemesi

--- CampusRide/Models/User.cs
+++ CampusRide/Models/User.cs
@@ -1,24 +1,34 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CampusRide.Models
 {
     public class User
     {
-        public int Id { get; set; }
+        [Required]
+        [Key]
+        [Phone]
+        public string? PhoneNumber { get; set; }
         [Required]
         public string? Name { get; set; } = string.Empty;
 		[Required]
 		public string? Surname { get; set; } = string.Empty;
-        public string? FullName { get; }
+        [Required]
+        public string? Gender {  get; set; } = string.Empty;
+        [Required]
+        [Range(18, 100)]
+        public int Age { get; set; }
 		[Required]
+        [EmailAddress]
 		public string? Email { get; set; } = string.Empty;
         [Required]
+        [PasswordPropertyText]
         public string? Password {  get; set; } = string.Empty;
+        public string? About { get; set; } = string.Empty;
         public DateTime RegistryTime { get; set; }
         public User()
         {
             RegistryTime = DateTime.Now;
-            FullName = Name + Surname;
         }
     }
 }
